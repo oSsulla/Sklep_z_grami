1fd1041d903dad824a0fd68985aa3578bb032133
oSsulla <osa2000osa@gmail.com>
2022-01-21 15:14:03 +0100

    Dodaj pliki projektów.

---
+++ Sklep_z_grami/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sklep_z_grami.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
